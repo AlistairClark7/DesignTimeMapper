2341fe9c8b0e3f775da99498903be43b7e610736
Alistair Clark <alistairclark7@gmail.com>
2016-11-30 13:49:10 +0000

    Added console application project with some skeleton implementation

---
+++ DesignTimeMapper/DesignTimeMapper/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignTimeMapper.Engine;
+using DesignTimeMapper.Engine.Settings;
+using Newtonsoft.Json;
+
+namespace DesignTimeMapper
+{
+    class Program
+    {
+        private const int ErrorSuccess = 0;
+        private const int ErrorInvalidFunction = 0x1;
+        private const int ErrorFileNotFound = 0x2;
+        private const int ErrorBadArguments = 0xA0;
+        private const int ErrorInvalidCommandLine = 0x667;
+
+        static int Main(string[] args)
+        {
+            if (args == null || args.Length != 1)
+            {
+                Console.Error.WriteLine("Please provide a path to the Design Time Mapper settings file");
+                return ErrorInvalidCommandLine;
+            }
+            
+            try
+            {
+                var path = args[0];
+
+                if (!File.Exists(path))
+                {
+                    Console.Error.WriteLine($"Could not find Design Time Mapper settings at path '{path}'");
+                    return ErrorFileNotFound;
+                }
+                    
+                var settingsText = File.ReadAllText(path);
+                var settings = JsonConvert.DeserializeObject<DtmSettings>(settingsText);
+
+                if (settings == null)
+                {
+                    Console.Error.WriteLine($"Could not parse contents of settings at path '{path}'");   
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Unexpected exception: {e}");
+
+                return ErrorInvalidFunction;
+            }
+
+            return ErrorSuccess;
+        }
+    }
+}
