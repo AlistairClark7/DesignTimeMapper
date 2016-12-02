6f491c97e2a366be4e64820b2d2c1cc01cc48ecd
Alistair Clark <alistairclark7@gmail.com>
2016-12-02 13:51:29 +0000

    Added AddressDto which maps from CivicAddress

---
+++ DesignTimeMapper/DesignTimeMapper.ExamplePocos/AddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Device.Location;
+using DesignTimeMapper.Engine.Attributes;
+
+namespace DesignTimeMapper.ExamplePocos
+{
+    [MapFrom(typeof(CivicAddress))]
+    public class AddressDto
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Building { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string StateProvince { get; set; }
+    }
+}
