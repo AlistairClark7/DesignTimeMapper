d7b94c8f2d874683762c5f0de38e08046c6b73cc
Alistair Clark <alistairclark7@gmail.com>
2016-12-13 12:51:03 +0000

    Added class with nested properties

---
+++ DesignTimeMapper/DesignTimeMapper.ExamplePocos/NestedClass.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignTimeMapper.ExamplePocos
+{
+    public class NestedClass
+    {
+        public string Property1 { get; set; }
+        public SimpleClass Nested { get; set; }
+    }
+}
---
+++ DesignTimeMapper/DesignTimeMapper.ExamplePocos/NestedClassDto.cs
@@ -0,0 +1,12 @@
+﻿using DesignTimeMapper.Attributes;
+
+namespace DesignTimeMapper.ExamplePocos
+{
+    [MapFrom(typeof(NestedClass))]
+    public class NestedClassDto
+    {
+        public string Property1 { get; set; }
+        public string NestedProperty1 { get; set; }
+        public string NestedProperty2 { get; set; }
+    }
+}
